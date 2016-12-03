7f00b73ecb4bc4f63fcbb8292b584ccf8f6eb06a
Sean Payne <seantpayne@gmail.com>
2016-12-03 10:10:39 -0800

    Updated to more reliably close tunnel connections

--- sshception.sh
+++ sshception.sh
@@ -79,9 +79,22 @@ else
   TUNNEL_CONNECTION_STRING="$TUNNEL_USERNAME@$TUNNEL_HOST"
 fi
 
-(ssh -M -S $CTL_SOCKET_UUID -fnNT -L $TUNNELING_PORT:$TARGET_HOST:$TARGET_PORT $TUNNEL_CONNECTION_STRING -p $TUNNEL_PORT && \
-  echo $CTL_SOCKET_UUID >> ~/.ssh/open_connections
-  ssh -S $CTL_SOCKET_UUID -O check $TUNNEL_CONNECTION_STRING && \
-  ssh -o 'UserKnownHostsFile /dev/null' $TARGET_USERNAME@localhost -p $TUNNELING_PORT  && \
-  ssh -S $CTL_SOCKET_UUID -O exit $TUNNEL_CONNECTION_STRING && \
-  sed -i.bak "/$CTL_SOCKET_UUID/d" ~/.ssh/open_connections) || failOut "Unable to establish tunnel. Verify the tunnel isn't still open in ~/.ssh/open_connections"
+ssh -M -S $CTL_SOCKET_UUID -fnNT -L $TUNNELING_PORT:$TARGET_HOST:$TARGET_PORT $TUNNEL_CONNECTION_STRING -p $TUNNEL_PORT || failOut "Unable to establish tunnel."
+
+echo "$CTL_SOCKET_UUID=$TUNNEL_CONNECTION_STRING" >> ~/.ssh/open_connections
+
+
+disconnect() {
+  ssh -S $CTL_SOCKET_UUID -O exit $TUNNEL_CONNECTION_STRING || failOut "Unable to clean up tunnel. Verify the tunnel isn't still open."
+  sed -i.bak "/$CTL_SOCKET_UUID/d" ~/.ssh/open_connections
+}
+
+failOutAndDisconnect() {
+  disconnect
+  failOut "$1"
+}
+
+(ssh -S $CTL_SOCKET_UUID -O check $TUNNEL_CONNECTION_STRING && \
+  ssh -o 'UserKnownHostsFile /dev/null' -o 'StrictHostKeyChecking=no' $TARGET_USERNAME@localhost -p $TUNNELING_PORT) || failOutAndDisconnect "Unable to establish tunnel."
+
+disconnect
